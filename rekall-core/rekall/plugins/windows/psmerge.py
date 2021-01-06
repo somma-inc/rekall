c960b96d607d1498b81fa092767a79cf7cf5f5d8
pil9 <ljp3326@naver.com>
2021-01-06 16:32:58 +0900

    feat: pslist_api, driver 모드 결과 값 기능 반영

--- rekall-core/rekall/plugins/windows/psmerge.py
+++ rekall-core/rekall/plugins/windows/psmerge.py
@@ -12,49 +12,7 @@ from rekall.plugins.windows import common
 from rekall.plugins.windows.filescan import PoolScanProcess
 
 
-class TestGetTokenInformation(unittest.TestCase):
 
-    def setUp(self) -> None:
-        pass
-
-    def tearDown(self) -> None:
-        pass
-
-    def seDebug(self):
-        try:
-            """SEDebug"""
-            flags = win32security.TOKEN_ADJUST_PRIVILEGES | win32security.TOKEN_QUERY
-            htoken = win32security.OpenProcessToken(win32api.GetCurrentProcess(), flags)
-            id = win32security.LookupPrivilegeValue(None, "seDebugPrivilege")
-            newPrivileges = [(id, win32security.SE_PRIVILEGE_ENABLED)]
-            win32security.AdjustTokenPrivileges(htoken, 0, newPrivileges)
-        except Exception as e:
-            print ('seDebug error')
-            pass 
-
-
-    def test_get_elevated(self):
-
-        self.seDebug()
-        
-
-        process_list=ProcessTree().process_map()
-
-        for pid in process_list:
-            try:
-                # print(process_list[i].name)
-                ph = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION | win32con.PROCESS_VM_READ, False, pid)
-                th = win32security.OpenProcessToken(ph, win32con.MAXIMUM_ALLOWED)
-
-                is_elevated=win32security.GetTokenInformation(th, win32security.TokenElevation)                        
-                elevation_type = win32security.GetTokenInformation(th, win32security.TokenElevationType)
-                
-                print(f"{process_list[pid].name}({pid})\t{is_elevated}\t{elevation_type}")
-
-            except Exception as e:
-                pass
-
-        # pass
 
 class GetTokenInformation:
     def __init__(self):
@@ -204,7 +162,7 @@ def md5sum(file_path: str, blocksize=8192):
     return hash_func.hexdigest()
 
 
-class PSMerge(common.WinScanner, GetTokenInformation):
+class PSMerge(common.WinScanner):
     name = "psmerge"
 
     table_header = [
@@ -220,6 +178,10 @@ class PSMerge(common.WinScanner, GetTokenInformation):
         dict(name="elevation_type", width=27),
         dict(name="create_time", width=24),
         dict(name="exit_time", width=24),
+        dict(name="psscan_driver", width=5),
+        dict(name="pslist_driver", width=5),
+        dict(name="pslist_api", width=5)
+        
     ]
 
     # Only bother to scan non paged pool by default.
@@ -236,7 +198,7 @@ class PSMerge(common.WinScanner, GetTokenInformation):
         # pslist_gen = self.session.plugins.pslist().filter_processes()
         for i in pslist.list_eprocess():
             pslist_data[i.pid.value]=i
-        
+        pslist_driver=list(pslist_data.keys())
         api_ps_list = ProcessTree()
         # print("[+]",pslist.list_eprocess())
         # These are virtual addresses.
@@ -246,9 +208,10 @@ class PSMerge(common.WinScanner, GetTokenInformation):
             known_eprocess.add(task)
             known_pids.add(task.UniqueProcessId)
             # print("[+]", task.Peb.ProcessParameters.ImagePathName)
-
+        pslist_api=list(ProcessTree().process_map().keys())
         # Scan each requested run in turn.
         psscan_result = []
+
         for run in self.generate_memory_ranges():
             # Just grab the AS and scan it using our scanner
             scanner = PoolScanProcess(session=self.session,
@@ -269,17 +232,30 @@ class PSMerge(common.WinScanner, GetTokenInformation):
 
                 if eprocess.UniqueProcessId in known_pids:
                     known += "P"
-
+                
 
                 # print(pslist.filter_processes(pids=eprocess.pid))
                 # print(eprocess.pid.value,"\n")
                 pid=eprocess.pid.value
-                is_elevated, elevated_type=self.token_map(pid)
+
+                
+
+                is_elevated, elevated_type=GetTokenInformation().token_map(pid)
                 if len(is_elevated) == 0:
                     is_elevated[pid]=[None,"Unknown"]
                     elevated_type[pid]=[None,"Unknown"]
                 # print(is_elevated, elevated_type)
                 # print(is_elevated[eprocess.pid.value][1])
+                is_pslist_api="True"
+                if pid not in pslist_api:
+                    is_pslist_api="False"
+
+                
+                is_pslist_driver="True"
+                if pid not in pslist_driver:
+                    is_pslist_driver="False"
+                    
+
                 try:
                     
                     data = dict(
@@ -288,16 +264,27 @@ class PSMerge(common.WinScanner, GetTokenInformation):
                         # offset_v=virtual_eprocess.obj_offset,
                         ppid=eprocess.InheritedFromUniqueProcessId,
                         # imagepath=eprocess.Peb.ProcessParameters.ImagePathName,                        
-                        is_elevated = is_elevated[pid][1],
-                        elevation_type = elevated_type[pid][1],
+                        is_elevated = is_elevated[pid][1] or '',
+                        elevation_type = elevated_type[pid][1] or '',
                         imagepath=pslist_data[pid].Peb.ProcessParameters.ImagePathName,
                         # pdb=eprocess.Pcb.DirectoryTableBase,
                         create_time=eprocess.CreateTime or '',
-                        exit_time=eprocess.ExitTime or ''
+                        exit_time=eprocess.ExitTime or '',
+                        psscan_driver="True",
+                        pslist_api=is_pslist_api,
+                        pslist_driver=is_pslist_driver
+
                 )
-                except KeyError:
+                    # print (data)
+                except Exception as e:
+                    # print(e)
+                    # print (data)
                     pass
+                    
                 
+                
+
+                psscan_result.append(pid)
                 # print("[+]", eprocess.Peb.ProcessParameters.ImagePathName)
                 # psscan_result.append(
                 #     data
@@ -307,61 +294,7 @@ class PSMerge(common.WinScanner, GetTokenInformation):
 
         print("\n\n\n")
 
-        for run in self.generate_memory_ranges():
-            # Just grab the AS and scan it using our scanner
-            scanner = PoolScanProcess(session=self.session,
-                                      profile=self.profile,
-                                      address_space=run.address_space)
-
-            for pool_obj, eprocess in scanner.scan(offset=run.start, maxlen=run.length):
-                if run.data["type"] == "PhysicalAS":
-                    # Switch address space from physical to virtual.
-                    virtual_eprocess = (
-                        pslist.virtual_process_from_physical_offset(eprocess))
-                else:
-                    virtual_eprocess = eprocess
-
-                known = ""
-                if virtual_eprocess in known_eprocess:
-                    known += "E"
-
-                if eprocess.UniqueProcessId in known_pids:
-                    known += "P"
-
-
-                # print(pslist.filter_processes(pids=eprocess.pid))
-                # print(eprocess.pid.value,"\n")
-                pid=eprocess.pid.value
-                is_elevated, elevated_type=self.token_map(pid)
-                if len(is_elevated) == 0:
-                    is_elevated[pid]=[None,"Unknown"]
-                    elevated_type[pid]=[None,"Unknown"]
-                # print(is_elevated, elevated_type)
-                # print(is_elevated[eprocess.pid.value][1])
-                try:
-                    
-                    data = dict(
-                        # a='F' if pool_obj.FreePool else "",
-                        offset_p=eprocess,
-                        # offset_v=virtual_eprocess.obj_offset,
-                        ppid=eprocess.InheritedFromUniqueProcessId,
-                        # imagepath=eprocess.Peb.ProcessParameters.ImagePathName,                        
-                        is_elevated = is_elevated[pid][1],
-                        elevation_type = elevated_type[pid][1],
-                        imagepath=pslist_data[pid].Peb.ProcessParameters.ImagePathName,
-                        # pdb=eprocess.Pcb.DirectoryTableBase,
-                        create_time=eprocess.CreateTime or '',
-                        exit_time=eprocess.ExitTime or ''
-                )
-                except KeyError:
-                    pass
-                
-                # print("[+]", eprocess.Peb.ProcessParameters.ImagePathName)
-                # psscan_result.append(
-                #     data
-                # )
-
-                yield data    
+       
         print(psscan_result)
         # pslist plugin 결과, api를 통해서 얻은 프로세스 목록 결과, psscan 결과
 
